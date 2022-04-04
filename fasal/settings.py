2037519edda913534e7833dc11796a7318426106
Omkar Koli <koliomkar363@gmail.com>
2022-04-04 13:42:52 +0530

    Added django-heroku

--- fasal/settings.py
+++ fasal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = '/login'
+
+django_heroku.settings(locals())
